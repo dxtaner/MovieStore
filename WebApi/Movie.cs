cef4de4ec4377104d9121b51a8f26b251e12e22c
Taner Özer <tanerozer16@gmail.com>
2022-09-09 17:27:13 +0300

    movie added

---
+++ WebApi/Movie.cs
@@ -0,0 +1,13 @@
+namespace WebApi{
+
+    public class Movie{
+        
+        public int Id { get; set; }
+        public string title { get; set; }
+        public int genreId { get; set; }
+        public int len{ get; set; }
+        public DateTime publishDate { get; set; }
+
+
+    }
+} 
