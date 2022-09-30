0fe02c6bbc2c69cc7d22f6dc75b5dd3b82c50d74
Taner Özer <tanerozer16@gmail.com>
2022-09-30 18:31:29 +0300

    editedprogramcs

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,13 +1,29 @@
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using WebApi.DBOperations;
+using WebApi.Middlewares;
+using WebApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
+ConfigurationManager configuration = builder.Configuration;
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+builder.Services.AddDbContext<MovieStoreDbContext>(options => options.UseInMemoryDatabase(databaseName: "MovieStoreDB"));
+builder.Services.AddSingleton<ILoggerService, ConsoleLogger>();
+// builder.Services.AddScoped<IMovieStoreDbContext>(provider => provider.GetService<MovieStoreDbContext>());
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    DataGenerator.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -16,10 +32,17 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseAuthentication();
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+app.UseCustomExceptionMiddle();
+
 app.MapControllers();
 
+
 app.Run();
+
+
