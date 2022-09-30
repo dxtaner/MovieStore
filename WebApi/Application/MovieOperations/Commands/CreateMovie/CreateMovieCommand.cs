3bb48abdc60e0ecccd0268482276a4ac17c4e258
Taner Özer <tanerozer16@gmail.com>
2022-09-30 19:20:17 +0300

    GetMovieDetailQueryValidator Added

---
+++ WebApi/Application/MovieOperations/Commands/CreateMovie/CreateMovieCommand.cs
@@ -0,0 +1,39 @@
+using AutoMapper;
+using WebApi.DBOperations;
+using WebApi.Entites;
+
+namespace WebApi.Application.MovieOperations.Commands.CreateMovie
+{
+    public class CreateMovieCommand
+    {
+        private readonly IMovieStoreDbContext _context;
+        private readonly IMapper _mapper;
+        public CreateMovieModel Model { get; set; }
+        public CreateMovieCommand(IMovieStoreDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public void Handle()
+        {
+            var _movie = _context.Movies.SingleOrDefault(x => x.Title == Model.Title);
+            if (_movie is not null)
+                throw new InvalidOperationException("Film mevcut.");
+
+            _movie = _mapper.Map<Movie>(Model);
+
+            _context.Movies.Add(_movie);
+            _context.SaveChanges();
+        }
+    }
+
+    public class CreateMovieModel
+    {
+        public string Title { get; set; }
+        public DateTime PublishDate { get; set; }
+        public int GenreID { get; set; }
+        public int DirectorID { get; set; }
+        public int Price { get; set; }
+    }
+}
