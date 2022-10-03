6d4765df1cee90c299151a424072b97ff416499c
Taner Özer <tanerozer16@gmail.com>
2022-10-03 16:36:36 +0300

    genres add

--- WebApi/Common/MappingProfile.cs
+++ WebApi/Common/MappingProfile.cs
@@ -1,4 +1,8 @@
 using AutoMapper;
+using WebApi.Application.GenreOperations.Commands.CreateGenre;
+using WebApi.Application.GenreOperations.Commands.UpdateGenre;
+using WebApi.Application.GenreOperations.Queries.GetGenreDetail;
+using WebApi.Application.GenreOperations.Queries.GetGenres;
 using WebApi.Application.MovieOperations.Queries.GetMovieDetail;
 using WebApi.Application.MovieOperations.Queries.GetMovies;
 using WebApi.Entities;
@@ -11,6 +15,13 @@ namespace WebApi.Common
         {
             CreateMap<Movie, GetMoviesModel>().ForMember(dest => dest.Genre, opt => opt.MapFrom(src => src.Genre.GenreName)).ForMember(x => x.Director, y => y.MapFrom(z => z.Director.Name + " " + z.Director.Surname));
             CreateMap<Movie, GetMovieDetailModel>().ForMember(dest => dest.Genre, opt => opt.MapFrom(src => src.Genre.GenreName)).ForMember(x => x.Director, y => y.MapFrom(z => z.Director.Name + " " + z.Director.Surname));
+            CreateMap<Genre, GetGenreModel>();
+            CreateMap<Genre, GenreDetailModel>();
+            CreateMap<Genre, UpdateGenreModel>();
+            CreateMap<Genre, CreateGenreModel>();
+
+
+
 
 
         }
