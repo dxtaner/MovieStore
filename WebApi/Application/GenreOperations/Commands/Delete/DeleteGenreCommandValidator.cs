afdf2dbdd769dbd136f9e1d47e820ed54ec0ff69
Taner Özer <tanerozer16@gmail.com>
2022-10-03 16:18:51 +0300

    DeleteGenreCommandValidator add

---
+++ WebApi/Application/GenreOperations/Commands/Delete/DeleteGenreCommandValidator.cs
@@ -0,0 +1,12 @@
+using FluentValidation;
+
+namespace WebApi.Application.GenreOperations.Commands.DeleteGenre
+{
+    public class DeleteGenreCommandValidator : AbstractValidator<DeleteGenreCommand>
+    {
+        public DeleteGenreCommandValidator()
+        {
+            RuleFor(x => x.GenreID).GreaterThan(0);
+        }
+    }
+}
