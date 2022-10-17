1ba23aa48855fd98872dec4e9d299a162228c813
Taner Özer <tanerozer16@gmail.com>
2022-10-17 14:19:50 +0300

    delete director settting added

--- WebApi/Application/DirectorOperations/Commands/Delete/DeleteDirectorCommand.cs
+++ WebApi/Application/DirectorOperations/Commands/Delete/DeleteDirectorCommand.cs
@@ -16,6 +16,9 @@ namespace WebApi.Application.DirectorOperations.Commands.DeleteDirector
             if (_Director is null)
                 throw new InvalidOperationException("Director bulunamadı.");
 
+            var _movie = _context.Movies.FirstOrDefault(x => x.DirectorID == DirectorID);
+            if (_movie is not null)
+                throw new InvalidOperationException("Director silinemez. Mevcut Filmi Var.");
 
             _context.Directors.Remove(_Director);
             _context.SaveChanges();
