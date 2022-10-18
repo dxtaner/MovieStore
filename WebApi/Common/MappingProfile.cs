1fbc8a19dc705bb704680dafb656e3bcf9dfc6bf
Taner Özer <tanerozer16@gmail.com>
2022-10-18 14:18:12 +0300

    edited mapping profile

--- WebApi/Common/MappingProfile.cs
+++ WebApi/Common/MappingProfile.cs
@@ -3,6 +3,10 @@ using WebApi.Application.ActorOperations.Commands.CreateActor;
 using WebApi.Application.ActorOperations.Commands.UpdateActor;
 using WebApi.Application.ActorOperations.Queries.GetActorDetail;
 using WebApi.Application.ActorOperations.Queries.GetActors;
+using WebApi.Application.BoughtMovieOperations.Commands.CreateBoughtMovie;
+using WebApi.Application.BoughtMovieOperations.Commands.UpdateBoughtMovie;
+using WebApi.Application.BoughtMovieOperations.Queries.GetBoughtMovieDetail;
+using WebApi.Application.BoughtMovieOperations.Queries.GetBoughtMovies;
 using WebApi.Application.CustomerOperations.Commands.CreateCustomer;
 using WebApi.Application.CustomerOperations.Commands.UpdateCustomer;
 using WebApi.Application.CustomerOperations.Queries.GetCustomerDetail;
@@ -27,23 +31,26 @@ namespace WebApi.Common
         {
             CreateMap<Movie, GetMoviesModel>().ForMember(dest => dest.Genre, opt => opt.MapFrom(src => src.Genre.GenreName)).ForMember(x => x.Director, y => y.MapFrom(z => z.Director.Name + " " + z.Director.Surname));
             CreateMap<Movie, GetMovieDetailModel>().ForMember(dest => dest.Genre, opt => opt.MapFrom(src => src.Genre.GenreName)).ForMember(x => x.Director, y => y.MapFrom(z => z.Director.Name + " " + z.Director.Surname));
-            CreateMap<Genre, GetGenreModel>();
-            CreateMap<Genre, GenreDetailModel>();
+            CreateMap<Genre, GetGenreModel>().ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.GenreName)).ForMember(x => x.Name, y => y.MapFrom(z => z.GenreId + " " + z.GenreName));
+            CreateMap<Genre, GenreDetailModel>().ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.GenreName)).ForMember(x => x.Name, y => y.MapFrom(z => z.GenreId + " " + z.GenreName));
             CreateMap<Genre, UpdateGenreModel>();
             CreateMap<Genre, CreateGenreModel>();
-            CreateMap<Actor, GetActorsModel>();
-            CreateMap<Actor, ActorDetailModel>();
+            CreateMap<Actor, GetActorsModel>().ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name)).ForMember(x => x.Name, y => y.MapFrom(z => z.Name + " " + z.Surname + " " + z.PlayedMovies));
+            CreateMap<Actor, ActorDetailModel>().ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name)).ForMember(x => x.Name, y => y.MapFrom(z => z.Name + " " + z.Surname + " " + z.PlayedMovies));
             CreateMap<Actor, UpdateActorModel>();
             CreateMap<Actor, CreateActorModel>();
-            CreateMap<Director, GetDirectorsModel>();
+            CreateMap<Director, GetDirectorsModel>().ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name)).ForMember(x => x.Name, y => y.MapFrom(z => z.Name + " " + z.Surname + " " + z.DirectedMovies));
+            CreateMap<Director, UpdateDirectorModel>().ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name)).ForMember(x => x.Name, y => y.MapFrom(z => z.Name + " " + z.Surname + " " + z.DirectedMovies));
             CreateMap<Director, DirectorDetailModel>();
-            CreateMap<Director, UpdateDirectorModel>();
             CreateMap<Director, CreateDirectorModel>();
-            CreateMap<Customer, GetCustomersModel>().ForMember(dest => dest.BoughtFilms, opt => opt.MapFrom(src => src.BoughtFilms));
-            CreateMap<Customer, CustomerDetailModel>();
+            CreateMap<Customer, GetCustomersModel>().ForMember(dest => dest.BoughtFilms, opt => opt.MapFrom(src => src.BoughtFilms)).ForMember(x => x.BoughtFilms, y => y.MapFrom(z => z.Name + " " + z.Surname + "" + z.FavMovieGenre));
+            CreateMap<Customer, CustomerDetailModel>().ForMember(dest => dest.BoughtFilms, opt => opt.MapFrom(src => src.BoughtFilms)).ForMember(x => x.BoughtFilms, y => y.MapFrom(z => z.Name + " " + z.Surname + " " + z.FavMovieGenre));
             CreateMap<Customer, UpdateCustomerModel>();
             CreateMap<Customer, CreateCustomerModel>();
-
+            CreateMap<BoughtMovie, GetBoughtMoviesModel>().ForMember(dest => dest.Customer, opt => opt.MapFrom(src => src.Customer)).ForMember(x => x.Customer, y => y.MapFrom(z => z.Movie + " " + z.Price + " " + z.BoughtMovieDate));
+            CreateMap<BoughtMovie, BoughtMovieDetailModel>().ForMember(dest => dest.Customer, opt => opt.MapFrom(src => src.Customer)).ForMember(x => x.Customer, y => y.MapFrom(z => z.Movie + " " + z.Price + " " + z.BoughtMovieDate));
+            CreateMap<BoughtMovie, UpdateBoughtMovieModel>();
+            CreateMap<BoughtMovie, CreateBoughtMovieModel>();
 
 
 
